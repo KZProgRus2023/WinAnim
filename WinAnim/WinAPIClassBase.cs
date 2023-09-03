c01aeccbe0c7e4ed18afad9be21051cd0e746a23
Кирилл Желиховский <KirillZhelihovskij@rambler.ru>
2023-09-03 21:47:46 +0300

    Добавьте файлы проекта.

---
+++ WinAnim/WinAPIClassBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using static AnimatedWindow.WinAPIClass;
+
+namespace AnimatedWindow
+{
+    public class WinAPIClassBase
+    {
+        /// summary
+        /// Анимация окна.
+        /// </summary>
+        /// <param name=name="hwnd">Окно.</param>
+        /// <param name="dwTime">Время.</param>
+        /// <param name="dwFlags">Тип анимации. Если в 
+        /// неуправляемом коде используется перечисление, то его 
+        /// нужно конвертировать в тип данных int.
+        /// </param>
+        /// <returns></returns>
+        [DllImportAttribute("user32.dll", EntryPoint = "AnimateWindow", SetLastError = true)]
+        public static extern bool AnimateWindow(Control ctrl, int dwTime, AnimateWindowFlags Flags)
+        {
+            return AnimateWindow(ctrl.Handle, dwTime, Flags);
+        }
+    }
+}
